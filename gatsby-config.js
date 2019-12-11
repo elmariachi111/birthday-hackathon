e82446f208eaeba89bd9e94d7fd59edc0b725b08
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-12-11 16:31:27 +0100

    foldername

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Birthday Hackathon`,
+        short_name: `birthday-hackathon`,
         start_url: `/`,
         background_color: `#222222`,
         theme_color: `#fff1d7`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
